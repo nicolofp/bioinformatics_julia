4f19b8549e95133b931ce5c9d85f2bb13d142885
nicolofp <nicolo.fp@gmail.com>
2024-01-17 16:23:55 -0500

    update outlier

--- outlier_analysis.jl
+++ outlier_analysis.jl
@@ -10,35 +10,42 @@ pheno = pheno[pheno.relation .== "",:]
 mirna = DataFrame(Arrow.Table(lmirna))
 
 dt = pheno[pheno.source_name_ch1 .== "Non-CVD control",vcat(3,7:9,11:14)]
-mirna = mirna[:,vcat("rn",dt.geo_accession)]
+#mirna = mirna[:,vcat("rn",dt.geo_accession)]
 mirna.rn = "miRNA" .* string.(1:2565) # Because we have multiple names
 Tmirna = permutedims(mirna,1);
 
 # load the detector
-KNN_outlier = @iload KNNDetector pkg=OutlierDetectionNeighbors
-
-
+KNN = @iload KNNDetector pkg=OutlierDetectionNeighbors
 
 # instantiate a detector with default parameters, returning scores
-knn = KNN()
+knn_outlier = KNN()
 
 # bind the detector to data and learn a model with all data
-knn_raw = machine(knn, X) |> fit!
+X = MLJ.table(Matrix{Float64}(Tmirna[:,2:2566]))
+knn_raw = machine(knn_outlier, X) |> fit!
 
 # transform data to raw outlier scores based on the test data; note that there
 # is no `predict` defined for raw detectors
-transform(knn_raw, rows=test)
+tmp1 = MLJ.transform(knn_raw)#, rows=test)
 
 # OutlierDetection.jl provides helper functions to normalize the scores,
 # for example using min-max scaling based on the training scores
-knn_probas = machine(ProbabilisticDetector(knn), X) |> fit!
+knn_probas = machine(ProbabilisticDetector(knn_outlier), X) |> fit!
 
 # predict outlier probabilities based on the test data
-predict(knn_probas, rows=test)
+tmp = MLJ.predict(knn_probas)#, rows=test)
 
-# OutlierDetection.jl also provides helper functions to turn scores into classes,
-# for example by imposing a threshold based on the training data percentiles
-knn_classifier = machine(DeterministicDetector(knn), X) |> fit!
+# Use malahnobis distance and test
+Mmirna = Matrix{Float64}(Tmirna[:,2:11])
 
-# predict outlier classes based on the test data
-predict(knn_classifier, rows=test)
+function mahalanobis_distace(X)
+    x = X .- mean(X, dims = 1) 
+    B = cov(X)   
+    d = sum((x * inv(B)) .* x, dims = 2)
+    return(d)     
+end
+
+y =mahalanobis_distace(Mmirna)
+Chisq(k) 
+sum(y .> quantile(Chisq(10), 0.95))
+
