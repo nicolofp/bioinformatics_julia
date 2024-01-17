cf6ee3128e0ec7f62bf95f51c40399f19bc798e4
nicolofp <nicolo.fp@gmail.com>
2024-01-17 13:51:43 -0500

    test out

--- outlier_analysis.jl
+++ outlier_analysis.jl
@@ -1,4 +1,4 @@
-using DataFrames, Statistics, LinearAlgebra
+using DataFrames, Statistics, LinearAlgebra, OutlierDetection
 using Distributions, StatsBase, Random, MLJ
 
 include("test_s3.jl");
@@ -14,4 +14,31 @@ mirna = mirna[:,vcat("rn",dt.geo_accession)]
 mirna.rn = "miRNA" .* string.(1:2565) # Because we have multiple names
 Tmirna = permutedims(mirna,1);
 
+# load the detector
+KNN_outlier = @iload KNNDetector pkg=OutlierDetectionNeighbors
 
+
+
+# instantiate a detector with default parameters, returning scores
+knn = KNN()
+
+# bind the detector to data and learn a model with all data
+knn_raw = machine(knn, X) |> fit!
+
+# transform data to raw outlier scores based on the test data; note that there
+# is no `predict` defined for raw detectors
+transform(knn_raw, rows=test)
+
+# OutlierDetection.jl provides helper functions to normalize the scores,
+# for example using min-max scaling based on the training scores
+knn_probas = machine(ProbabilisticDetector(knn), X) |> fit!
+
+# predict outlier probabilities based on the test data
+predict(knn_probas, rows=test)
+
+# OutlierDetection.jl also provides helper functions to turn scores into classes,
+# for example by imposing a threshold based on the training data percentiles
+knn_classifier = machine(DeterministicDetector(knn), X) |> fit!
+
+# predict outlier classes based on the test data
+predict(knn_classifier, rows=test)
